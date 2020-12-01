af3290f44a4bb85a8f5752670a73ee86fe589bfa
acydcross <acydcross@hotmail.com>
2020-12-01 16:02:01 -0700

    Added feed.vue and tweet.vue

---
+++ src/components/Tweet.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="tweet-container">
+        <h3>{{ tweetObject.username }}</h3>
+        <p>{{ tweetObject.content }}</p>
+        <h5>{{ tweetObject.createdAt }}</h5>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "page-tweet",
+        props: {
+            tweetObject: {
+                type: Oject,
+                required: true
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
